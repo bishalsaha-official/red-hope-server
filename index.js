92b8d9c98e478e5d28494332ee27fa1f92480aee
bishalsaha-official <cse.bishalsaha@gmail.com>
2025-08-09 11:18:12 +0600

    Implement post api for contacts

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 require('dotenv').config()
 const app = express()
@@ -10,6 +11,51 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('RedHope is running')
 })
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jxyklbs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const contactCollection = client.db("RedHope").collection('contacts')
+
+
+    // Contacts APi
+    app.post('/contacts', async (req, res)=>{
+      const contact = req.body;
+      const result = await contactCollection.insertOne(contact)
+      res.send(result)
+    })
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.listen(port, () => {
   console.log(`Red Hope is Running on ${port}`)
